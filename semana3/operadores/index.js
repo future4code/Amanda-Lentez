658c54b2e6b7bef8a4ea62164f3339d4927be74e
Amanda Lentez <amanda.a.lentz@gmail.com>
2021-02-23 22:26:43 -0300

    exercício operações

---
+++ semana3/operadores/index.js
@@ -0,0 +1,130 @@
+/*Exercícios de Interpretação*/
+
+/*Exercício 1:
+10
+10 5*/
+
+/*Exercício 2:
+10 10 10*/
+
+
+/*Exercícios de Escrita de Código*/
+
+/*Exercício 1:*/
+/*let nome
+let idade
+console.log(typeof nome)
+console.log(typeof idade)*/
+/*Foi impresso o tipo "undefined", pois não foi atribuído 
+valor algum as variáveis criadas (nome e idade).*/
+/*let nome = prompt ("Qual o seu nome?")
+let idade = prompt ("Qual a sua idade?")
+console.log(typeof nome)
+console.log(typeof idade)*/
+/*Foi solicitado ao usuário o nome e posteriormente a idade.
+Foi impresso o tipo de variável "string", pois o valor atribuído a variável
+foi no formato de texto (entre "").*/
+/*console.log("Olá ", nome, ", você tem ", idade, " anos.")*/
+
+/*Exercício 2*/
+/*let nome = prompt ("1. Qual é o seu nome?")
+let idade = prompt ("2. Qual é a sua idade?")
+let serie = prompt ("3. Qual é a sua série preferida?")
+let filme = prompt ("4. Qual seu filme preferido?")
+let covid = prompt ("5. Você faz quarentena?")
+
+console.log("1. Qual é o seu nome?")
+console.log("Resposta: ", nome)
+
+console.log("2. Qual é a sua idade?")
+console.log("Resposta: ", idade)
+
+console.log("3. Qual é a sua série preferida?")
+console.log("Resposta: ", serie)
+
+console.log("4. Qual seu filme preferido?")
+console.log("Resposta: ", filme)
+
+console.log("5. Você faz quarentena?")
+console.log("Resposta: ", covid)*.
+
+/*Exercício 3*/
+/*let favFoods = ["espaghetti", "lasanha", "salada", "sushi", "strogonoff"]
+console.log(favFoods)
+console.log("Essas são minhas comidas preferidas:")
+console.log(favFoods[0])
+console.log(favFoods[1])
+console.log(favFoods[2])
+console.log(favFoods[3])
+console.log(favFoods[4])
+
+let userFavFood = prompt ("Qual é a sua comida preferida?")
+console.log("Essas são minhas comidas preferidas:")
+console.log(favFoods[0])
+console.log(userFavFood)
+console.log(favFoods[2])
+console.log(favFoods[3])
+console.log(favFoods[4])*/
+
+/*Exercício 4*/
+/*let perguntas = ["Já tomou cloroquina?", "Já tomou ivermectina?", "Gosta de suco de laranja?"]
+
+let respostas = [false, false, false]
+
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])*/
+
+//Exercicio 1//
+/*const bool3 = true
+
+let resultado = true && false
+console.log("a. ", false)
+
+resultado = true && false && true
+console.log("b. ", false)
+
+resultado = !resultado && (true || true)
+console.log("c. ", true)
+
+console.log("e. ", typeof boolean)*/
+
+
+//Exercício 2//
+
+
+/*let array
+console.log('a. ', [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+
+array = null
+console.log('b. ', [])
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', 11)
+
+let i = 0
+console.log('d. ', array[3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0])
+
+array[i+1] = 19
+console.log('e. ', array[0+1])
+
+const valor = array[i+6]
+console.log('f. ', array[0+6])*/
+
+//Exercício 1//
+
+/*const idade = prompt ("1. Qual é o seu nome?")
+const idadeAmigo = prompt ("2. Qual é a sua idade?")
+
+const diferenca = Number(idadeAmigo) - Number(idade)
+console.log("A diferença de idades é:  ", diferenca)
+
+const maiorMenor = Number(idadeAmigo) < Number(idade)
+console.log("A sua idade é maior do que a to seu amigo: ", maiorMenor)*/
+
+//Exercício 2//
+
+/*const numero = prompt ("Insira um número par")
+let restoDivisao = numero%2
+console.log("O resto da divisão é: ", restoDivisao)*/
+
