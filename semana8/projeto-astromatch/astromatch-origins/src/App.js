d5b1032064ee75774eb4c78fba24c646d1521cff
Amanda Lentez <amanda.a.lentz@gmail.com>
2021-04-08 21:26:18 -0300

    day 2

--- semana8/projeto-astromatch/astromatch-origins/src/App.js
+++ semana8/projeto-astromatch/astromatch-origins/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Chama a Abin
         </a>
       </header>
     </div>
